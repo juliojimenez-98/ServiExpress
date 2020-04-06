62954e54e2cedaac05e40576eee55f662f5dcbcd
Marco <marcoastorga.g@gmail.com>
2020-04-06 12:52:19 -0400

    up

--- js/app.js
+++ js/app.js
@@ -5,9 +5,9 @@ function ingre(){
          if (email2=="admin") {
             setTimeout("location.href='admin.html'", 0);
          } else if (email2=="empleado") {
-            setTimeout("location.href='empleados.html'", 0);
+            setTimeout("location.href='empleadolog/empleadolog.html'", 0);
         } else if (email2=="cliente") {
-            setTimeout("location.href='clientes.html'", 0);
+            setTimeout("location.href='clientelog.html'", 0);
         } else if (email2=="proveedor") {
             setTimeout("location.href='proveedores.html'", 0);
          } else {
